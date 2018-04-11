d91493415ae53fbb4fc583953659d1d5aa6911fe
jmccown1 <30505376+jmccown1@users.noreply.github.com>
2018-04-11 16:24:40 -0500

    Added buttons to let user choose which columns/rows in files to import into the program.

--- DataTypeDialogBox.py
+++ DataTypeDialogBox.py
@@ -23,7 +23,7 @@ class Ui_Dialog(QFileDialog):
     def setupUi(self, Dialog):
 
         Dialog.setObjectName("Dialog")
-        Dialog.resize(284, 216)
+        Dialog.resize(354, 306)
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(Dialog)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.verticalLayout = QtWidgets.QVBoxLayout()
@@ -43,11 +43,14 @@ class Ui_Dialog(QFileDialog):
         self.FreqRadioB.setObjectName("FreqRadioB")
         self.FreqRadioB.clicked.connect(self.isFrequency)
         self.pushButton_2 = QtWidgets.QPushButton(self.groupBox)
-        self.pushButton_2.setGeometry(QtCore.QRect(130, 90, 110, 32))
+        self.pushButton_2.setGeometry(QtCore.QRect(10, 180, 110, 32))
+        self.groupBox2 = QtWidgets.QGroupBox(self.groupBox)
+        self.groupBox2.setGeometry(QtCore.QRect(145, 80, 161, 131))
+        self.groupBox2.setObjectName("groupBox2")
 
         self.pushButton_2.setObjectName("pushButton_2")
         self.pushButton = QtWidgets.QPushButton(self.groupBox)
-        self.pushButton.setGeometry(QtCore.QRect(130, 40, 110, 32))
+        self.pushButton.setGeometry(QtCore.QRect(150, 40, 110, 32))
         self.pushButton.setObjectName("pushButton")
 
         # Open file button listener
@@ -60,6 +63,40 @@ class Ui_Dialog(QFileDialog):
         self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName("buttonBox")
         self.verticalLayout_2.addWidget(self.buttonBox)
+
+        # Allow users to select columns and rows to have data run on
+        self.rowBeginSpinBox = QtWidgets.QSpinBox(self.groupBox2)
+        self.rowBeginSpinBox.setGeometry(QtCore.QRect(10, 50, 53, 24))
+        self.rowBeginSpinBox.setMinimum(0)
+        self.rowBeginSpinBox.setMaximum(999)
+        self.rowBeginSpinBox.setObjectName("spinBox")
+        self.rowsToLabel = QtWidgets.QLabel(self.groupBox2)
+        self.rowsToLabel.setGeometry(QtCore.QRect(75, 50, 61, 20))
+        self.rowsToLabel.setObjectName("rowsToLabel")
+        self.rowsLabel = QtWidgets.QLabel(self.groupBox2)
+        self.rowsLabel.setGeometry(QtCore.QRect(50, 30, 56, 13))
+        self.rowsLabel.setObjectName("rowsLabel")
+        self.rowEndSpinBox = QtWidgets.QSpinBox(self.groupBox2)
+        self.rowEndSpinBox.setGeometry(QtCore.QRect(100, 50, 53, 24))
+        self.rowEndSpinBox.setMinimum(0)
+        self.rowEndSpinBox.setMaximum(999)
+        self.rowEndSpinBox.setObjectName("spinBox_2")
+        self.colsToLabel = QtWidgets.QLabel(self.groupBox2)
+        self.colsToLabel.setGeometry(QtCore.QRect(75, 100, 51, 20))
+        self.colsToLabel.setObjectName("colsToLabel")
+        self.colBeginSpinBox = QtWidgets.QSpinBox(self.groupBox2)
+        self.colBeginSpinBox.setGeometry(QtCore.QRect(10, 100, 53, 24))
+        self.colBeginSpinBox.setMinimum(0)
+        self.colBeginSpinBox.setMaximum(999)
+        self.colBeginSpinBox.setObjectName("spinBox_3")
+        self.colEndSpinBox = QtWidgets.QSpinBox(self.groupBox2)
+        self.colEndSpinBox.setGeometry(QtCore.QRect(100, 100, 53, 24))
+        self.colEndSpinBox.setMinimum(0)
+        self.colEndSpinBox.setMaximum(999)
+        self.colEndSpinBox.setObjectName("spinBox_4")
+        self.colsLabel = QtWidgets.QLabel(self.groupBox2)
+        self.colsLabel.setGeometry(QtCore.QRect(50, 80, 71, 16))
+        self.colsLabel.setObjectName("colsLabel")
 
         self.retranslateUi(Dialog)
         self.buttonBox.accepted.connect(Dialog.accept)
@@ -120,6 +157,11 @@ class Ui_Dialog(QFileDialog):
         self.FreqRadioB.setText(_translate("Dialog", "Frequency"))
         self.pushButton_2.setText(_translate("Dialog", "Manual Input"))
         self.pushButton.setText(_translate("Dialog", "Open File"))
+        self.rowsToLabel.setText(_translate("Dialog", "to"))
+        self.rowsLabel.setText(_translate("Dialog", "Rows"))
+        self.colsToLabel.setText(_translate("Dialog", "to"))
+        self.colsLabel.setText(_translate("Dialog", "Columns"))
+        self.groupBox2.setTitle(_translate("Dialog", "Select Data for Analysis:"))
 
 if __name__ == "__main__":
     import sys
